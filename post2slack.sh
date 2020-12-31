70b40c81a43f8fa5d96f651e82ef786024de9299
cbolk <cristiana.bolchini@polimi.it>
2020-12-31 17:29:40 +0100

    Delete post2slack.sh

--- post2slack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WEBHOOK=https://hooks.slack.com/services/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
-mytext=$1
-
-curl -X post -H 'content-type: application/json' --data = "{'text':'$mytext : sunny: '}" $WEBHOOK
-
